ed477a6ec8b669991efc6c7f750c5c673315fbc8
李宗校 <158638447@qq.com>
2022-09-19 15:44:48 +0800

    project init

---
+++ src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <Card
+      title="请修改标题"
+      :bordered="false"
+      :headStyle="{ color: 'rgba(51, 51, 51, 0.85)' }"
+      style="width: 100%"
+    >
+      <Button type="primary">按钮</Button>
+    </Card>
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from "vue";
+import { Card, Button } from "ant-design-vue";
+export default defineComponent({
+  name: "AboutView",
+  components: { Card, Button },
+  setup() {
+    return {};
+  },
+});
+</script>
+<style lang="less" scoped></style>
