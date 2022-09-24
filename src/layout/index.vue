aca186d74e379be4aadc29dd875b7f2faf51b873
李宗校 <158638447@qq.com>
2022-09-24 20:52:41 +0800

    新增路由

---
+++ src/layout/index.vue
@@ -0,0 +1,24 @@
+<template>
+ <div>
+   <Card
+    title='请修改标题'
+    :bordered='false'
+    :headStyle="{ color: 'rgba(51, 51, 51, 0.85)' }"
+    style='width: 100%'
+    >
+   </Card>
+ </div>
+</template>
+<script  lang="ts">
+import { defineComponent } from 'vue';
+import { Card } from 'ant-design-vue';
+ export default defineComponent({
+   name:'请输入名字',
+   components: { Card },
+  setup() {
+   return {};
+   },
+  });
+</script>
+<style lang="less" scoped>
+</style>
